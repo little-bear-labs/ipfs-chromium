4588e96cb7d1cec895f44efb04dec23e11d6026a
John Turpish <john@littlebearlabs.io>
2023-03-28 12:43:36 -0400

    Base 36 works. Generic algo someday. Now to verify...

--- TODO.md
+++ TODO.md
@@ -1,17 +1,13 @@
-* Build scripting to download & setup a Chromium tree on your behalf
 * Blog & video for the uninitiated
-* Shared Scheduler
 * Intent to prototype
-* ipns resolution
+* base36
+* IPNS validation
 * Decompose UnixFsPathResolver
 * Docker builds verifying every documented build approach
 * Scan all known nodes of a HAMT for index.html before deciding to require everything for a listing
 * Unit tests coverage for ipfs_client, using GTest
 * Do real URLLoader management for gateway requests (SetPriority, pause, etc.)
 * Use SetPriority to modify Scheduler parameters
-* Validation
-    - Other hashes for blocks
-    - IPNS signatures
 * User settings
 * ChromiumStyle() (accessors_only())
 * Persist promote/demote (perhaps integrated with user settings)
@@ -21,3 +17,4 @@
 * prefetch
 * frecency to GC block cache
 * Enforce maximum block size
+* Discovery doesn't really belong in gateway_requests.cc
--- library/src/libp2p/multi/multibase_codec/codecs/base36.cc
+++ library/src/libp2p/multi/multibase_codec/codecs/base36.cc
@@ -1,18 +1,56 @@
 #include <libp2p/multi/multibase_codec/codecs/base36.hpp>
 
+#include <vocab/i128.h>
+
+#include <algorithm>
+
 namespace det = libp2p::multi::detail;
 
 namespace {
-using namespace std::literals;
-// constexpr auto kUpperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"sv;
-// constexpr auto kLowerAlphabet = "abcdefghijklmnopqrstuvwxyz234567"sv;
+constexpr double kLengthRatio = 0.646240625;  // log(36)/log(256)
+
+std::int_least16_t digit_value(char digit) {
+  if (digit < '0') {
+    return -1;
+  } else if (digit <= '9') {
+    return digit - '0';
+  } else if (digit < 'A') {
+    return -2;
+  } else if (digit <= 'Z') {
+    return (digit - 'A') + 10;
+  } else if (digit < 'a') {
+    return -3;
+  } else if (digit <= 'z') {
+    return (digit - 'a') + 10;
+  } else {
+    return -4;
+  }
+}
+int operator*(int a, ipfs::Byte b) {
+  return a * static_cast<std::uint_least8_t>(b);
+}
 }  // namespace
 
-std::string det::encodeBase36Lower(ipfs::ByteView bytes) {
+std::string det::encodeBase36Lower(ipfs::ByteView) {
   std::abort();
 }
 
-auto det::decodeBase36(std::string_view string)
+auto det::decodeBase36(std::string_view str_b36)
     -> ipfs::expected<common::ByteArray, BaseError> {
-  std::abort();
+  common::ByteArray out;
+  out.resize(std::ceil(static_cast<double>(str_b36.size()) * kLengthRatio),
+             ipfs::Byte{});
+  for (auto digit : str_b36) {  // chunk) {
+    int val = digit_value(digit);
+    if (val < 0) {
+      return ipfs::unexpected<BaseError>{BaseError::INVALID_BASE36_INPUT};
+    }
+    auto mod_byte = [&val](auto& b) {
+      val += 36 * b;
+      b = static_cast<ipfs::Byte>(val & 0xFF);
+      val >>= 8;
+    };
+    std::for_each(out.rbegin(), out.rend(), mod_byte);
+  }
+  return out;
 }
