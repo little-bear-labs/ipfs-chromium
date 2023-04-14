f6263f3e5d913191b6c0bc28ced44c8bbbee8ef2
John Turpish <john@littlebearlabs.io>
2023-04-14 16:16:53 -0400

    squash! squash! welp?
    checkpoint

---
+++ cmake/upload.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from glob import glob
+from google.cloud import storage
+from platform import uname
+from sys import argv
+
+import distro
+
+client = storage.Client('arcas-326114')
+bucket = client.get_bucket('chromium-dev')
+sys = distro.id()
+if not sys:
+    sys = uname().system
+prefix = f'{argv[1]}/{sys}'
+for splat in ["*.deb", "*.rpm", "*.dmg"]:
+    for f in glob(splat):
+        print("Uploading",f,f"to chromium-dev/{prefix}")
+        blob = bucket.blob(f"{prefix}/{f}")
+        blob.upload_from_filename(f)
