04dc807604e4ddec418d9f01ff7d9618cdb8e3c7
John Turpish <john@littlebearlabs.io>
2023-06-13 14:09:44 -0600

    Read user-set env. See IPFS_GATEWAY . Started Conan build.

--- cmake/setup.cmake
+++ cmake/setup.cmake
@@ -1,3 +1,4 @@
+set(SETUP_CMAKE_INCLUDED 1)
 set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR};${CMAKE_CURRENT_BINARY_DIR};${CMAKE_MODULE_PATH})
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_BINARY_DIR}")
 include(${CMAKE_CXX_COMPILER_ID})
@@ -8,7 +9,11 @@ if(CLANG_TIDY_EXE)
 #    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXE}" "--config-file=${CMAKE_CURRENT_SOURCE_DIR}/.clang-tidy")
 endif()
 include("${CMAKE_CURRENT_LIST_DIR}/python.cmake")
-if(HAVE_PIP)
+if(INSIDE_CONAN)
+    message(STATUS "CMake invoked by Conan - assuming the outer Conan did all the dependency work.")
+elseif(NOT HAVE_PIP)
+    message(WARNING "Could not find python 3. Can't use gn or conan. You're on your own for Chromium & dependencies.")
+else()
     set(CONAN_CMAKE_SILENT_OUTPUT 1)
     execute_process(
         COMMAND conan config home
@@ -30,14 +35,23 @@ if(HAVE_PIP)
         #How incredibly obnoxious is it that conan 2 dropped support for these envs?
         file(WRITE "${conan_home}/global.conf" core.net.http:timeout=$ENV{CONAN_REQUEST_TIMEOUT})
     endif()
+    file(COPY_FILE
+      "${CMAKE_CURRENT_LIST_DIR}/conanfile.txt"
+      "${CMAKE_CURRENT_BINARY_DIR}/conanfile.txt"
+      )
+    find_package(Doxygen)
+    if (NOT DOXYGEN_FOUND)
+        message(WARNING "Will attempt to get doxygen from Conan, which doesn't work on some systems like Github's ubuntu-latest runner.")
+        file(APPEND
+          "${CMAKE_CURRENT_BINARY_DIR}/conanfile.txt"
+          doxygen/1.9.4
+        )
+    endif()
     execute_process(
-        COMMAND conan install ${CMAKE_CURRENT_LIST_DIR} --output-folder=${CMAKE_CURRENT_BINARY_DIR} --build=missing --settings build_type=${CMAKE_BUILD_TYPE} --generator CMakeDeps
+        COMMAND conan install ${CMAKE_CURRENT_BINARY_DIR} --output-folder=${CMAKE_CURRENT_BINARY_DIR} --build=missing --settings build_type=${CMAKE_BUILD_TYPE} --generator CMakeDeps
         COMMAND_ERROR_IS_FATAL ANY
         WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
         )
-
-else()
-    message(WARNING "Could not find python 3. Can't use gn or conan. You're on your own for Chromium & dependencies.")
 endif()
 
 function(with_vocab target)
