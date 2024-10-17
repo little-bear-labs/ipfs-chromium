2b625985e0318ca1578c8c285fd9922eb129c9e8
John Turpish <john@littlebearlabs.io>
2024-10-17 09:45:22 -0400

    squash! Don't apt install lcov in a github runner as the version keeps floating around and breaking things.
    checkpoint

--- .github/ut.sh
+++ .github/ut.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -ex
 
 echo Install dependencies
-          sudo apt-get update
-          sudo apt-get install --yes cmake ninja-build binutils doxygen graphviz libc6{,-dev} valgrind pipx
+    sudo apt-get update
+    sudo apt-get install --yes \
+        cmake \
+        ninja-build \
+        doxygen \
+        graphviz \
+        valgrind \
+        pipx \
+        lcov # yes still install, to get dependencies
 
 wget https://github.com/linux-test-project/lcov/releases/download/v2.1/lcov-2.1.tar.gz
 tar xzvf lcov-2.1.tar.gz
@@ -12,11 +19,15 @@ pipx ensurepath
 pipx install conan
 npm install -g @marp-team/marp-cli
 
+set +x
 echo Show versions:
-g++ --version
-clang++ --version
-conan --version
-lcov --version
+for p in g++ gcc clang{++,} lcov conan
+do
+    which "${p}"
+    "${p}" --version
+done
+set -x
+
 conan profile detect
 
 echo Configure
